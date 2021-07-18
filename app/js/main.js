a0135e43ac475934671f80cc370d6dc40012f236
Dias <said.ahmetov@list.ru>
2021-07-18 22:03:41 +0600

    feat: completed work section with slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
-$(function () {
-
-});
+window.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.app__container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+  });
+}, false);
