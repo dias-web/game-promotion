4ae403831f7903995326d2ca16a79b95a42c94dd
Dias <said.ahmetov@list.ru>
2021-07-26 07:25:24 +0600

    feat: completed all section

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,23 @@
 window.addEventListener('DOMContentLoaded', function () {
-  const swiper = new Swiper('.app__container', {
+  const swiper1 = new Swiper('.app__container', {
     slidesPerView: 3,
     spaceBetween: 30,
     loop: true,
     autoplay: {
       delay: 2000,
+      pauseOnMouseEnter: true,
+
+    },
+  });
+
+  const swiper2 = new Swiper('.expertise__container', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      pauseOnMouseEnter: true,
+
     },
   });
 }, false);
